b93a48ae3fc13afa7d82aeb4e28c1b9def3779ff
Md-Bari <bari15-5598@diu.edu.bd>
2025-02-18 10:16:18 +0600

    save it

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp();
 }
 
 class LabClass extends StatelessWidget {
   const LabClass05({super.key});
-  
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title:"Basic Flutter UI -02",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Basic Flutter UI part 02"),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+      ),
+      body: Row(
+        children: [
+          Text(
+            "Hi, ",
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 30,
+            ),
+          ),
+          Text(
+            "this is ",
+            style:TextStyle(
+              color: Colors.green,
+              fontSize: 50,
+            ),
+          ),
+          Text(
+            "Shitol of DIU",
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 70,
+            ),
+          )
+        ],
+      ),
+      ),
+      
+    )
+
+  }
+
 }
