dedf39263327b1ec8d064e7e0702d7d081b16527
Md Rofiqul Bari <bari15-5598@diu.edu.bd>
2025-02-18 11:21:02 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,58 +7,37 @@ void main() {
 class LabClass05 extends StatelessWidget {
   const LabClass05({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title:"Basic Flutter UI -02",
+      title: "Basic Flutter UI -02",
       home: Scaffold(
         appBar: AppBar(
           title: Text("Basic Flutter UI part 02"),
           backgroundColor: Colors.blue,
           centerTitle: true,
-      ),
-      body: Row(
-        children: [
-          Text(
-            "4.0",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 25,
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          
+          child:Container(
+            height: 600,
+            child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: List.generate (
+              100,
+              (index) =>Icon(
+             Icons.star,
+             size: index.toDouble(),
+             color: const Color.fromARGB(255, 120, 222, 125),
+              ),
+
             ),
           ),
-          Icon(
-            Icons.star,
-            size: 60,
-            
-            color: const Color.fromARGB(255, 255, 217, 0),
-          ),
-          Icon(
-            Icons.star,
-            size: 60,
-            color: const Color.fromARGB(255, 34, 230, 0),
-          ),
-          Icon(
-            Icons.star,
-            size: 60,
-            color: const Color.fromARGB(255, 172, 0, 0),
-          ),
-          Icon(
-            Icons.star,
-            size: 60,
-            color: const Color.fromARGB(255, 102, 132, 97),
-          ),
-          Icon(
-            Icons.star_border_outlined,
-            size: 60,
-            color: const Color.fromARGB(255, 102, 132, 97),
           ),
           
-        ],
+        ),
       ),
-      
-      ),
-      
     );
-
   }
-
 }
